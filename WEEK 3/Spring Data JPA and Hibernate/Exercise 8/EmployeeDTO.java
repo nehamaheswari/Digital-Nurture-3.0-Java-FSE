eca4ca9eb9377a447ba33e8b4dfc445ef9baee91
Neha Maheswari <142196851+nehamaheswari@users.noreply.github.com>
2024-08-14 15:41:50 +0530

    Create EmployeeDTO.java

---
+++ WEEK 3/Spring Data JPA and Hibernate/Exercise 8/EmployeeDTO.java
@@ -0,0 +1,11 @@
+public class EmployeeDTO {
+
+    private String name;
+    private String department;
+
+    public EmployeeDTO(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+
+}
