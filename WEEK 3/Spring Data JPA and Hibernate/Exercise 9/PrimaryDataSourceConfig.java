d3805596e4899a656cf83e83a320252124f1c38f
Neha Maheswari <142196851+nehamaheswari@users.noreply.github.com>
2024-08-14 15:48:50 +0530

    Update PrimaryDataSourceConfig.java

--- WEEK 3/Spring Data JPA and Hibernate/Exercise 9/PrimaryDataSourceConfig.java
+++ WEEK 3/Spring Data JPA and Hibernate/Exercise 9/PrimaryDataSourceConfig.java
@@ -1,29 +1,31 @@
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
-@EnableJpaRepositories(
-    basePackages = "com.example.primary.repository",
-    entityManagerFactoryRef = "primaryEntityManagerFactory",
-    transactionManagerRef = "primaryTransactionManager"
-)
 public class PrimaryDataSourceConfig {
 
-    @Bean(name = "primaryDataSource")
-    @ConditionalOnProperty(name = "spring.datasource.primary.url")
-    public DataSource primaryDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/primarydb");
-        dataSource.setUsername("root");
-        dataSource.setPassword("password");
-        return dataSource;
+    @Bean(name = "primaryEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean primaryEntityManagerFactory(
+            @Qualifier("primaryDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan(new String[]{"com.example.primary.model"});
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        return em;
     }
 
+    @Bean(name = "primaryTransactionManager")
+    public JpaTransactionManager primaryTransactionManager(
+            @Qualifier("primaryEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
 }
