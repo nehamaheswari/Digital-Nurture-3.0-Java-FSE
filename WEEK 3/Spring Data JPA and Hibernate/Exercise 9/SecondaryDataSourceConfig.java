d8b709f1330eecf1412a31636574156180859222
Neha Maheswari <142196851+nehamaheswari@users.noreply.github.com>
2024-08-14 15:49:10 +0530

    Update SecondaryDataSourceConfig.java

--- WEEK 3/Spring Data JPA and Hibernate/Exercise 9/SecondaryDataSourceConfig.java
+++ WEEK 3/Spring Data JPA and Hibernate/Exercise 9/SecondaryDataSourceConfig.java
@@ -1,29 +1,31 @@
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
-@EnableJpaRepositories(
-    basePackages = "com.example.secondary.repository",
-    entityManagerFactoryRef = "secondaryEntityManagerFactory",
-    transactionManagerRef = "secondaryTransactionManager"
-)
 public class SecondaryDataSourceConfig {
 
-    @Bean(name = "secondaryDataSource")
-    @ConditionalOnProperty(name = "spring.datasource.secondary.url")
-    public DataSource secondaryDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/secondarydb");
-        dataSource.setUsername("root");
-        dataSource.setPassword("password");
-        return dataSource;
+    @Bean(name = "secondaryEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean secondaryEntityManagerFactory(
+            @Qualifier("secondaryDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan(new String[]{"com.example.secondary.model"});
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        return em;
     }
 
+    @Bean(name = "secondaryTransactionManager")
+    public JpaTransactionManager secondaryTransactionManager(
+            @Qualifier("secondaryEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
 }
