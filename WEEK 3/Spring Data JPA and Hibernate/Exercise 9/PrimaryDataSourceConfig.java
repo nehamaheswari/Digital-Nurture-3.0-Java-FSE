7ad5ce77c60ff5415bcbc894fd00b806010f758e
Neha Maheswari <142196851+nehamaheswari@users.noreply.github.com>
2024-08-14 15:45:55 +0530

    Create PrimaryDataSourceConfig.java

---
+++ WEEK 3/Spring Data JPA and Hibernate/Exercise 9/PrimaryDataSourceConfig.java
@@ -0,0 +1,29 @@
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(
+    basePackages = "com.example.primary.repository",
+    entityManagerFactoryRef = "primaryEntityManagerFactory",
+    transactionManagerRef = "primaryTransactionManager"
+)
+public class PrimaryDataSourceConfig {
+
+    @Bean(name = "primaryDataSource")
+    @ConditionalOnProperty(name = "spring.datasource.primary.url")
+    public DataSource primaryDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/primarydb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password");
+        return dataSource;
+    }
+
+}
